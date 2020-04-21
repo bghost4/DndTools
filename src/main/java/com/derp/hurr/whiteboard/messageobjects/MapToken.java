30784c0d9d7d25f7c4a0bc74e6b37cf8321c94f6
Delbert Martin IV <dmartin@rheonix.com>
2020-04-21 10:24:31 -0400

    added base MapToken class

---
+++ src/main/java/com/derp/hurr/whiteboard/messageobjects/MapToken.java
@@ -0,0 +1,27 @@
+package com.derp.hurr.whiteboard.messageobjects;
+
+import javafx.scene.Node;
+
+import java.util.UUID;
+
+public class MapToken implements Drawable {
+    @Override
+    public Node generateNode() {
+        return null;
+    }
+
+    @Override
+    public boolean hasAnimation() {
+        return false;
+    }
+
+    @Override
+    public UUID getID() {
+        return null;
+    }
+
+    @Override
+    public void setID(UUID id) {
+
+    }
+}
