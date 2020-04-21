475f754653c7d085d0ae8f27fb497be6aa4aec5e
Delbert Martin IV <dmartin@rheonix.com>
2020-04-21 11:27:54 -0400

    moved to java 14

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module OSDungeon.main {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports com.derp.hurr.whiteboard;
+}
